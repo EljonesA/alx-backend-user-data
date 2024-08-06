33097327cea97888ee7d6f893123bc6c9f6de0f7
Eljones <eljones.odongo@gmail.com>
2024-08-06 17:10:57 +0300

    Request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -32,7 +32,11 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Returns the authorization header from the request """
-        return None
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Returns the current user """
