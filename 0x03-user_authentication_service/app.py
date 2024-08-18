4530f9323c1b212ba834d293d50d7b66defce57b
Eljones <eljones.odongo@gmail.com>
2024-08-18 16:46:50 +0300

    User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -75,5 +75,21 @@ def logout():
     abort(403)
 
 
+@app.route("/profile", methods=["GET"])
+def profile():
+    """GET /profile route to return user profile information."""
+    session_id = request.cookies.get("session_id")
+
+    if not session_id:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        return jsonify({"email": user.email}), 200
+
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
